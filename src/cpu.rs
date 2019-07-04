a7139aa47a4a81d3e16ceef873374523b02c29f2
Joshua Suskalo <joshua@suskalo.org>
2019-07-03 19:48:14 -0500

    Add wrapping to the math ops

--- src/cpu.rs
+++ src/cpu.rs
@@ -7,6 +7,7 @@ use rand;
 use rand::Rng;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::u8;
 
 const STACK_TOP: usize = 0x1FF;
 pub const PROGRAM_START: usize = 0x200;
@@ -147,7 +148,7 @@ impl Cpu {
                 self.registers.vx[idx.0 as usize] = byte;
             }
             Instruction::Add(idx, byte) => {
-                self.registers.vx[idx.0 as usize] += byte;
+                self.registers.vx[idx.0 as usize] = self.registers.vx[idx.0 as usize].wrapping_add(byte);
             }
             Instruction::LoadVx(x, y) => {
                 self.registers.vx[x.0 as usize] = self.registers.vx[y.0 as usize];
@@ -176,7 +177,7 @@ impl Cpu {
                     } else {
                         0
                     };
-                self.registers.vx[x.0 as usize] -= self.registers.vx[y.0 as usize];
+                self.registers.vx[x.0 as usize] = self.registers.vx[x.0 as usize].wrapping_sub(self.registers.vx[y.0 as usize]);
             }
             Instruction::ShiftRight(idx) => {
                 self.registers.vx[0xF] = if self.registers.vx[idx.0 as usize] & 0x1 == 1 {
@@ -196,7 +197,7 @@ impl Cpu {
                     };
 
                 self.registers.vx[x.0 as usize] =
-                    self.registers.vx[y.0 as usize] - self.registers.vx[y.0 as usize];
+                    self.registers.vx[y.0 as usize].wrapping_sub(self.registers.vx[x.0 as usize]);
             }
             Instruction::ShiftLeft(idx) => {
                 self.registers.vx[0xF] = if (self.registers.vx[idx.0 as usize] >> 7 & 0x1) == 1 {
