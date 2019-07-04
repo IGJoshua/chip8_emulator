7f53bce0e96c36ff1ac26e7ad114e19a3cae1da9
Joshua Suskalo <joshua@suskalo.org>
2019-07-03 19:55:04 -0500

    Add commandline arguments for filename and cycles per second

--- src/lib.rs
+++ src/lib.rs
@@ -23,11 +23,11 @@ use std::time::{Duration, SystemTime};
 const NANOS_PER_MILLI: u64 = 1000000;
 const NANOS_PER_SECOND: u64 = NANOS_PER_MILLI * 1000;
 
-pub fn start_emulator(filename: String, cycles_per_second: u64) {
+pub fn start_emulator(filename: &str, cycles_per_second: u64) {
     let mut ram = Memory::new();
 
     {
-        let file = fs::read(&filename).unwrap();
+        let file = fs::read(filename).unwrap();
         ram.write(cpu::PROGRAM_START, &file[..]);
     }
 
@@ -38,13 +38,8 @@ pub fn start_emulator(filename: String, cycles_per_second: u64) {
     let iteration_duration = Duration::from_nanos(NANOS_PER_SECOND / 60);
     let cycles_per_iteration = cycles_per_second / 60;
 
-    println!("Desired millis per frame: {}", iteration_duration.as_millis());
-
-    let mut frame_start = SystemTime::now();
     loop {
-        println!("Millis for last frame: {}", frame_start.elapsed().unwrap().as_millis());
-        frame_start = SystemTime::now();
-        //let frame_start_time = SystemTime::now();
+        let frame_start = SystemTime::now();
 
         for _ in 0..cycles_per_iteration + 1 {
             let instr = load_next_instruction(&mut cpu, &ram);
@@ -53,14 +48,8 @@ pub fn start_emulator(filename: String, cycles_per_second: u64) {
 
         cpu.step_clocks();
 
-        println!("Millis for instruction execution: {}", frame_start.elapsed().unwrap().as_millis());
-
         let events = window.draw_display(&display).unwrap();
-        println!("Millis for window drawing: {}", frame_start.elapsed().unwrap().as_millis());
-
         window.process_events(events);
-
-        println!("Millis for events: {}", frame_start.elapsed().unwrap().as_millis());
 
         let frame_duration = frame_start.elapsed().unwrap();
         if frame_duration < iteration_duration {
